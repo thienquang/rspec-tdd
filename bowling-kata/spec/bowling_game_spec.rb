dbc679bcd1b62bbc2ccd7986e4102753460ccec6
thienquang <quangthien@outlook.com>
2016-03-18 21:33:25 +0700

    bowling game kata

---
+++ bowling-kata/spec/bowling_game_spec.rb
@@ -0,0 +1,30 @@
+require 'bowling_game.rb'
+describe BowlingGame do  
+  let (:game) { BowlingGame.new }
+
+  it 'scores a gutter game' do
+    game.pins([0] * 20)
+    expect(game.score).to eq(0)
+  end
+
+  it 'scores a game of 1s' do
+    game.pins([1] * 20)
+    expect(game.score).to eq(20)
+  end
+
+  it 'scores agame with spare' do
+    game.pins([4, 6, 5] + [0]*17)
+    expect(game.score).to eq(20)
+  end
+
+  it 'scores a game with strike' do
+    game.pins([10, 3,5] + [0]*17)
+    expect(game.score).to eq(26)
+  end
+
+  it 'score a perfect game' do
+    game.pins([10]*12)
+    expect(game.score).to eq(300)
+  end
+
+end
--- bowling-kata/spec/playground_spec.rb
+++ bowling-kata/spec/playground_spec.rb
@@ -1,8 +1,8 @@
 # require_relative '../lib/playground.rb'  -- Donot need because rspec auto look in to lib folder
-require 'playground'
-describe Playground do
-  context 'when there are no children' do
-    it 'is quite boring place when there are no children'
-    it 'is empty when there are no children'
-  end
-end
+# require 'playground'
+# describe Playground do
+#   context 'when there are no children' do
+#     it 'is quite boring place when there are no children'
+#     it 'is empty when there are no children'
+#   end
+# end
